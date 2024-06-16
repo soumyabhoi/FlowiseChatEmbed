c81bf13f03e1fb3f07afe2fa07a64001f8807c3b
soumyabhoi <soumyaranjanbhoi2@gmail.com>
2024-06-16 16:15:57 +0100

    remove badge branding

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1061,12 +1061,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
               />
             )}
           </div>
-          <Badge
-            footer={props.footer}
-            badgeBackgroundColor={props.badgeBackgroundColor}
-            poweredByTextColor={props.poweredByTextColor}
-            botContainer={botContainer}
-          />
+          
         </div>
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
